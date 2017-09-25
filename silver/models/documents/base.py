7ae252a03f3a3bb29e1e1a9435caab5dae76e500
Ioana Vasi <vasi_ioandre@yahoo.com>
2017-09-25 16:33:40 +0300

    WIP, saved from broken system

--- silver/models/documents/base.py
+++ silver/models/documents/base.py
@@ -96,10 +96,11 @@ class BillingDocumentManager(models.Manager):
         queryset = queryset.select_related('customer', 'provider')
 
         if self.model.kind == 'Invoice':
-            queryset = queryset.prefetch_related('invoice_entries__product_code')
+            queryset = queryset.prefetch_related('invoice_entries', 'invoice_entries__product_code')
 
         if self.model.kind == 'Proforma':
-            queryset = queryset.prefetch_related('proforma_entries__product_code',
+            queryset = queryset.prefetch_related('proforma_entries',
+                                                 'proforma_entries__product_code',
                                                  'proforma_entries__invoice')
         return queryset
 
